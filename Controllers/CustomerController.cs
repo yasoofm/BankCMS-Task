b1bcfc1c6c39f8eb805ba75594ff0e91039f67bf
yousef mandani <ytmandani@outlook.com>
2024-04-16 15:28:47 +0300

    submission

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using BankCMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankCMS.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Customer[] customers = [
+                new Customer(Id: 1, Name: "yousef", Number: "12345678", Age: 28),
+                new Customer(Id: 2, Name: "othman", Number: "12345678", Age: 25),
+                new Customer(Id: 3, Name: "fawaz", Number: "12345678", Age: 22),
+            ];
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+        public IActionResult Details(int id)
+        {
+            var customer = customers.Single(c => c.Id == id);
+            if (customer == null)
+            {
+                return View("index");
+            }
+            return View(customer);
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace BankCMS.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public int Age { get; set; }
+        public int Id { get; set; }
+
+        public Customer(int Id, string Name, string Number, int Age)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Number = Number; 
+            this.Age = Age;
+        }
+    }
+}
